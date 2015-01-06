c1bf1da4205586597caa695b7c08563fb065efa7
Amira Jelassi <jelassi.amira85@gmail.com>
2015-01-06 18:42:08 +0100

    add Security

---
+++ src/Baquaras/TestBundle/Services/LadAuth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Baquaras\TestBundle\Services;
+ 
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+ 
+class LadAuth
+{
+    
+    
+    
+}
+
