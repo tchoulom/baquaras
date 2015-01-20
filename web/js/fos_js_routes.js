5ff8034225975e7a9e883b9f5094c9c3e794e414
Amira Jelassi <jelassi.amira85@gmail.com>
2015-01-20 17:52:29 +0100

    dd generate js

---
+++ web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"listerUsers":{"tokens":[["variable","\/","[^\/]++","sort"],["text","\/utilisateurs"]],"defaults":{"sort":null},"requirements":[],"hosttokens":[]}},"prefix":"","host":"localhost","scheme":"http"});
